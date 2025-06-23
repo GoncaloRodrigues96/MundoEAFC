7378068e4ffc7dad890d2f3e41c90d9f792c9c70
GoncaloRodrigues96 <48450976+GoncaloRodrigues96@users.noreply.github.com>
2025-06-23 12:17:26 +0100

    Primeiro commit do blog

---
+++ script.js
@@ -0,0 +1,3 @@
+// script.js – Ficheiro pronto para uso futuro.
+// Neste momento não contém código activo, mas está incluído em ambas as páginas.
+// Podes usar este ficheiro para funções JS partilhadas entre admin.html e index.html.
