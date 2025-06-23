67b294e88e9f63653a122c0ed1f7af14a3ac8879
GoncaloRodrigues96 <48450976+GoncaloRodrigues96@users.noreply.github.com>
2025-06-23 15:06:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-// script.js – Ficheiro pronto para uso futuro.
-// Neste momento não contém código activo, mas está incluído em ambas as páginas.
-// Podes usar este ficheiro para funções JS partilhadas entre admin.html e index.html.
